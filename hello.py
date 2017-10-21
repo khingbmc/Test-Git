03aa7e351eccde12909a79e81ee0e4f44b2040de
Ratanon Jantanasukon <60070079@kmitl.ac.th>
2017-10-21 14:27:11 +0700

    haha

--- hello.py
+++ hello.py
@@ -1,2 +1,13 @@
-while 1:
-	print("khing")
+"""Fibo"""
+def main(num):
+    """This is function main"""
+    print(recur(num))
+def recur(number):
+    """This is function recursion"""
+    if number == 1 or number == 0:
+        return number
+    else:
+        check = recur(max(number - 1, 0))+recur(max(number - 2, 0))
+        return check
+main(int(input()))
+"""blahblahblahb"""
