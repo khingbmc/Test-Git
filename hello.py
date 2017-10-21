1b4a5344fe171235f9c930b7e44dcfdaad0a923c
Ratanon Jantanasukon <60070079@kmitl.ac.th>
2017-10-21 13:38:33 +0700

    add fibo

--- hello.py
+++ hello.py
@@ -1,3 +1,12 @@
-check = input()
-print("Hello"+check)
-"""khing sudlhor"""
+"""Fibo"""
+def main(num):
+    """This is function main"""
+    print(recur(num))
+def recur(number):
+    """This is function recursion"""
+    if number == 1 or number == 0:
+        return number
+    else:
+        check = recur(max(number - 1, 0))+recur(max(number - 2, 0))
+        return check
+main(int(input()))
